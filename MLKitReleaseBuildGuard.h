2d18182485e266ddc1be29adcfaea97b6b2ffd5d
chixm <devcxm@gmail.com>
2026-06-24 12:24:56 +0800

    Add MLKit v8 arm64 simulator patched pods

---
+++ MLKitReleaseBuildGuard.h
@@ -0,0 +1,8 @@
+#ifndef MLKIT_RELEASE_BUILD_GUARD_H
+#define MLKIT_RELEASE_BUILD_GUARD_H
+
+#if !defined(DEBUG)
+#error "Patched framework is only allowed in DEBUG builds: MLKIT_6A1F8D2C_NON_DEBUG_BUILD_FORBIDDEN"
+#endif
+
+#endif /* MLKIT_RELEASE_BUILD_GUARD_H */
